c1c92ba577f10adcc00820b239cce31ba5385f6b
Dat Ha <datfalcon@gmail.com>
2019-09-17 13:38:48 -0400

    commented code

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,9 +1,18 @@
+# sudoku solver algorithm
+# Dat Ha
+# 2019/09/16
+
+
+# generates a class that acts as the sudoku game board
+# this class has many feature that will quickly let the solver access to the data on the grid
 class Board:
 
 	def __init__(self, data):
+		# get the starting grid configuration and set it into a 2d array
 		self.grid = [[int(j) for j in list(data[i*9:i*9+9])] for i in range(9)]
 
 
+	# print the grid representation of the game board
 	def Print(self):
 		print("Sudoku grid")
 		for row in range(9):
@@ -18,14 +27,18 @@ class Board:
 		print()	
 
 
+	# get all of the numbers in a row from left to right
 	def GetRow(self, row):
 		return [self.grid[row][i] for i in range(9)]
 
 
+	# get all the numbers in a column from top to bottom
 	def GetColumn(self, column):
 		return [self.grid[i][column] for i in range(9)]
 
 
+	# get all numbers in a square, left to right, top to bottom
+	# this function also returns the starting row and column coordinates in the grid of that square
 	def GetSquare(self,square):
 		row0 = (square//3) *3
 		column0 = (square%3)*3
@@ -35,6 +48,8 @@ class Board:
 		return data, row0, column0
 
 
+	# when a value at a coordinate only has 1 possible number,
+	# then write that number to the grid (because it has to be that number)
 	def UpdateWithPossibleNumbers(self, possibleNumbers):
 		for row in range(9):
 			for column in range(9):
@@ -42,6 +57,8 @@ class Board:
 					self.grid[row][column] = possibleNumbers[row][column][0]
 		
 
+	# output the solved sudo grid in the same format as the data input
+	# this is used to compare with the solution (which is also in this format)
 	def GetOneLineStringOutput(self):
 		output = ''
 		for row in self.grid:
@@ -54,6 +71,9 @@ class Board:
 
 class SudokuSolver:
 
+	# possible numbers array
+	# at the start, every square can be any number from 1-9 inclusively
+	# then, as we refine everything, the possibilities reduce until there is only 1 possible number left
 	possibleNumbers = [
 		[[1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9],  [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9],  [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9],],
 		[[1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9],  [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9],  [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9],],
@@ -72,12 +92,14 @@ class SudokuSolver:
 	def __init__(self, board):
 		self.board = board
 
+		# set the possible nunmbers of the starting values of the grid in
 		for row in range(9):
 			for column in range(9):
 				if self.board.grid[row][column] != 0:
 					self.possibleNumbers[row][column] = [self.board.grid[row][column]]
 
 
+	# it will reduce the possibility of a number based on a list of numbers that that number can't be
 	def ReducePossibilities(self, rowIndex, columnIndex, impossibleNumbers):
 		if len(self.possibleNumbers[rowIndex][columnIndex]) > 1:
 			for toBeRemoved in impossibleNumbers:
@@ -87,6 +109,7 @@ class SudokuSolver:
 					pass
 				
 
+	# prints all the possible numbers (used in debuging)
 	def PrintPossibleNumbers(self):
 		for row in range(9):
 			for column in range(9):
@@ -95,6 +118,7 @@ class SudokuSolver:
 		print('-------------------------------------')
 
 
+	# gets all the numbers that a certain value can't be
 	def GetImpossibleNumbers(self, iterable):
 		impossibleNumbers = []
 		iterableSet = set(iterable)
@@ -104,6 +128,7 @@ class SudokuSolver:
 		return impossibleNumbers
 
 	
+	# check if we all the cases have a value (this does not mean that the board is legit solved)
 	def IsSolved(self):
 		value = False
 		for row in self.board.grid:
@@ -111,20 +136,30 @@ class SudokuSolver:
 		return not value
 	
 
+	# solve the board
 	def Solve(self, maxCycle=100000):
+		
+		# this will only cycle a maximum number of times
+		# if it can't be solved, then it will fail, and there is an escape built in so the algorithm doesn't run forever
 		for i in range(maxCycle):
+			
+			# general structure of solving algorithm
+			# check what numbers are impossible for each cells in a row that aren't sure yet
+			# go through each of those cells and reduce their possibilities with the numbers found from above
+			# do this for all rows, columns, and squares
+			# rinse and repeat until it is solved
+
 			for rowIndex in range(9):
 				row = self.board.GetRow(rowIndex)
 				impossibleNumbers = self.GetImpossibleNumbers(row)
 				for columnIndex in range(9):
 					self.ReducePossibilities(rowIndex, columnIndex, impossibleNumbers)
-			
+
 			for columnIndex in range(9):
 				column = self.board.GetColumn(columnIndex)
 				impossibleNumbers = self.GetImpossibleNumbers(column)
 				for rowIndex in range(9):
 					self.ReducePossibilities(rowIndex, columnIndex, impossibleNumbers)
-
 			
 			for squareIndex in range(9):
 				square, row0, column0 = self.board.GetSquare(squareIndex)
@@ -133,12 +168,16 @@ class SudokuSolver:
 					for columnIndex in range(3):
 						self.ReducePossibilities(row0 + rowIndex, column0 + columnIndex, impossibleNumbers)
 			
-			self.board.UpdateWithPossibleNumbers(self.possibleNumbers)
-			if self.IsSolved():
-				return i+1
-		else:
+
+			self.board.UpdateWithPossibleNumbers(self.possibleNumbers) # update	the board if we solved any cells
+			
+			if self.IsSolved(): # exit solve function once we solved it
+				return i+1      # return the numbers of cycle needed
+
+
+		else: # if after the set max we haven't solved it yet, escape the function with error message
 			print("Failed to solve Sudoku in under %s moves, please raise max in order for alrorithm to run longer" %maxCycle)
-			return -1
+			return -1 # return -1 to indicate error
 
 
 
